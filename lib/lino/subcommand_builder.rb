74379dffdd642d2dcdf1500ddb6804293eb3b02d
Stephen Anderson <stephen.anderson@appstech.com.au>
2021-03-29 16:23:47 +1100

    Rubocop cleanup of subcommand_builder
    Standard spacing and replacement suggestions applied.

--- lib/lino/subcommand_builder.rb
+++ lib/lino/subcommand_builder.rb
@@ -6,52 +6,53 @@ module Lino
     include Lino::Utilities
 
     class <<self
-      def for_subcommand subcommand
+      def for_subcommand(subcommand)
         SubcommandBuilder.new(subcommand: subcommand)
       end
     end
 
-    def initialize(
-        subcommand: nil,
-        switches: [])
+    def initialize(subcommand: nil, switches: [])
       @subcommand = subcommand
       @switches = Hamster::Vector.new(switches)
     end
 
     def with_option(switch, value, separator: nil, quoting: nil)
-      with(switches: @switches.add({
-          components: [switch, value],
-          separator: separator,
-          quoting: quoting
-      }))
+      with(
+        switches: @switches.add(
+          {
+            components: [switch, value],
+            separator: separator,
+            quoting: quoting
+          }
+        )
+      )
     end
 
     def with_flag(flag)
-      with(switches: @switches.add({components: [flag]}))
+      with(switches: @switches.add({ components: [flag] }))
     end
 
     def build(option_separator, option_quoting)
       components = [
-          @subcommand,
-          map_and_join(@switches,
-              &(quote_with(option_quoting) >> join_with(option_separator)))
+        @subcommand,
+        map_and_join(
+          @switches,
+          &(quote_with(option_quoting) >> join_with(option_separator))
+        )
       ]
-
-      components
-          .reject { |item| item.empty? }
-          .join(' ')
+      components.reject(&:empty?).join(' ')
     end
 
     private
 
-    def with **replacements
+    def with(**replacements)
       SubcommandBuilder.new(**state.merge(replacements))
     end
 
     def state
       {
-          subcommand: @subcommand,
-          switches: @switches
+        subcommand: @subcommand,
+        switches: @switches
       }
     end
   end
