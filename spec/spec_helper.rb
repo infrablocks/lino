ae622ea5b6274bc7f9f9cf5f3610b3b67b7f70e6
Toby Clemson <tobyclemson@gmail.com>
2021-04-02 16:21:36 +0100

    Configure simplecov.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.start do
+  enable_coverage :branch
+  minimum_coverage line: 100, branch: 100
+end
+
 require 'bundler/setup'
 require 'lino'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
+  config.filter_run_when_matching :focus
   config.example_status_persistence_file_path = '.rspec_status'
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
