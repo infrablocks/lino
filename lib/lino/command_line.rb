1a8b9df17888047bdea97fae586b75baef41cc78
Toby Clemson <tobyclemson@gmail.com>
2024-07-05 21:22:32 +0100

    Refactor command line internals.

--- lib/lino/command_line.rb
+++ lib/lino/command_line.rb
@@ -3,8 +3,17 @@
 require 'open4'
 
 module Lino
-  # rubocop:disable Metrics/ClassLength
   class CommandLine
+    COMPONENTS = [
+      %i[environment_variables],
+      %i[command],
+      %i[options after_command],
+      %i[subcommands],
+      %i[options after_subcommands],
+      %i[arguments],
+      %i[options after_arguments]
+    ].freeze
+
     attr_reader :command,
                 :subcommands,
                 :options,
@@ -35,31 +44,21 @@ module Lino
     end
 
     def array
-      array_component_paths
-        .collect do |path|
-        components_at_path(array_formatted_components, path)
-      end
-        .reject(&:empty?)
-        .flatten
+      format_components(:array, COMPONENTS.drop(1)).flatten
     end
 
     alias to_a array
 
     def string
-      string_component_paths
-        .collect do |path|
-        components_at_path(string_formatted_components, path)
-      end
-        .reject(&:empty?)
-        .join(' ')
+      format_components(:string, COMPONENTS).join(' ')
     end
 
     alias to_s string
 
     def ==(other)
-      self.class == other.class &&
-        state == other.state
+      self.class == other.class && state == other.state
     end
+
     alias eql? ==
 
     def hash
@@ -90,103 +89,30 @@ module Lino
     end
 
     def env
-      @environment_variables.to_h
+      @environment_variables.to_h(&:array)
     end
 
-    def array_component_paths
-      [
-        %i[command],
-        %i[options after_command],
-        %i[subcommands],
-        %i[options after_subcommands],
-        %i[arguments],
-        %i[options after_arguments]
-      ]
+    def format_components(format, paths)
+      paths
+        .collect { |p| components_at_path(formatted_components(format), p) }
+        .compact
+        .reject(&:empty?)
     end
 
-    def string_component_paths
-      [%i[environment_variables]].concat(array_component_paths)
-    end
-
-    def array_formatted_components
-      @array_formatted_components ||= {
+    def formatted_components(format)
+      {
+        environment_variables: @environment_variables.map(&format),
         command: @command,
-        options: array_formatted_options,
-        subcommands: array_formatted_subcommands,
-        arguments: array_formatted_arguments
+        options: @options
+          .group_by(&:placement)
+          .map { |p, o| [p, o.map(&format)] },
+        subcommands: @subcommands.map(&format),
+        arguments: @arguments.map(&format)
       }
-    end
-
-    def string_formatted_components
-      @string_formatted_components ||= {
-        environment_variables: string_formatted_environment_variables,
-        command: @command,
-        options: string_formatted_options,
-        subcommands: string_formatted_subcommands,
-        arguments: string_formatted_arguments
-      }
-    end
-
-    def string_formatted_environment_variables
-      @environment_variables.map(&:string).join(' ')
-    end
-
-    def array_formatted_options_with_placement(placement)
-      options_with_placement(placement).map(&:array)
-    end
-
-    def string_formatted_options_with_placement(placement)
-      options_with_placement(placement).map(&:string)
-    end
-
-    def array_formatted_options
-      %i[
-        after_command
-        after_subcommands
-        after_arguments
-      ].inject({}) do |options, placement|
-        options
-          .merge({ placement =>
-                     array_formatted_options_with_placement(placement) })
-      end
-    end
-
-    def string_formatted_options
-      %i[
-        after_command
-        after_subcommands
-        after_arguments
-      ].inject({}) do |options, placement|
-        options
-          .merge({ placement =>
-                     string_formatted_options_with_placement(placement) })
-      end
-    end
-
-    def array_formatted_subcommands
-      @subcommands.map(&:array)
-    end
-
-    def string_formatted_subcommands
-      @subcommands.map(&:string).join(' ')
-    end
-
-    def array_formatted_arguments
-      @arguments.map(&:array)
-    end
-
-    def string_formatted_arguments
-      @arguments.map(&:string).join(' ')
     end
 
     def components_at_path(components, path)
       path.inject(components) { |c, p| c && c[p] }
     end
-
-    def options_with_placement(placement)
-      @options.select { |o| o.placement == placement }
-    end
   end
-
-  # rubocop:enable Metrics/ClassLength
 end
